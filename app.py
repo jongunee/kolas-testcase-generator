448b20865810ae31d476e8a658514f2d97d49b86
jongunee <jejeoppa@naver.com>
2023-12-21 13:40:11 +0900

    all additional setting will be created

--- app.py
+++ app.py
@@ -36,36 +36,6 @@ test_units_data = {
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        # Device 정보와 Endpoint, Authentication 정보
-        device_info = {
-            'product_name': request.form.get('product_name', 'Unknown Product'),
-            'product_version': request.form.get('product_version', 'Unknown Version'),
-            'product_category': request.form.get('product_category', 'Unknown Category'),
-            'company_name': request.form.get('company_name', 'Unknown Company'),
-            'endpoint': request.form.get('endpoint', ''),
-            'username': request.form.get('username', ''),
-            'password': request.form.get('password', '')
-        }
-
-        selected_test_cases = {group: request.form.getlist(group) for group in test_units_data}
-
-        # 추가 설정 데이터 수집
-        additional_settings = {}
-        for group in test_units_data:
-            for case in test_units_data[group]:
-                if case in selected_test_cases[group]:
-                    settings = {}
-                    for key in request.form:
-                        if key.startswith(case + "_"):  # 예: sTC010_NsIdA
-                            settings[key[len(case)+1:]] = request.form[key]
-                    additional_settings[case] = settings
-
-        xml_content = generate_xml(device_info, selected_test_cases, additional_settings)
-        response = make_response(xml_content)
-        response.headers["Content-Disposition"] = "attachment; filename=deviceConfiguration.xml"
-        return response
-
     # 각 테스트 케이스에 대한 설정 파일 존재 여부 확인
     settings_exists = {
         case: os.path.isfile(f'templates/settings/{case}.html')
@@ -73,35 +43,60 @@ def index():
         for case in test_units_data[group]
     }
     
+    if request.method == 'POST':
+        # Device 정보와 Endpoint, Authentication 정보
+        device_info = {
+            'ProductName': request.form.get('product_name', 'Unknown Product'),
+            'ProductVersion': request.form.get('product_version', 'Unknown Version'),
+            'ProductCategory': request.form.get('product_category', 'Unknown Category'),
+            'CompanyName': request.form.get('company_name', 'Unknown Company'),
+            'endpoint': request.form.get('endpoint', ''),
+            'username': request.form.get('username', ''),
+            'password': request.form.get('password', '')
+        }
+
+        selected_test_cases = {group: request.form.getlist(group) for group in test_units_data}
+
+        additional_config_test_cases = {
+            group: [case for case in test_units_data[group] if os.path.isfile(f'templates/settings/{case}.html')]
+            for group in test_units_data
+        }
+
+        # 추가 설정 데이터 수집
+        additional_settings = {}
+        for group in test_units_data:
+            for case in test_units_data[group]:
+                if settings_exists[case]:
+                    settings = {}
+                    for key in request.form:
+                        if key.startswith(case + "_"):  # 예: sTC010_NsIdA
+                            settings[key[len(case)+1:]] = request.form[key]
+                    additional_settings[case] = settings
+
+        xml_content = generate_xml(device_info, selected_test_cases, additional_config_test_cases, additional_settings)
+
+        
+        response = make_response(xml_content)
+        response.headers["Content-Disposition"] = "attachment; filename=deviceConfiguration.xml"
+        return response
+    
     return render_template('index.html', test_units_data=test_units_data, settings_exists=settings_exists)
 
-
-
-def collect_additional_settings(request_form):
-    additional_settings = {}
-    for key in request_form:
-        if key.startswith("sTC"):
-            parts = key.split('_')
-            if len(parts) == 2:
-                test_case, param = parts
-                additional_settings.setdefault(test_case, {})[param] = request_form[key]
-    return additional_settings
-
-def generate_xml(device_info, selected_cases, additional_settings):
+def generate_xml(device_info, selected_cases, additional_config_test_cases, additional_settings):
     root = Element('configuration')
     root.append(Comment('This file is a auto generated file from interoperability web portal'))
 
     # DeviceInformation 추가 (endpoint, username, password 제외)
     device_info_el = SubElement(root, 'DeviceInformation')
-    for key in ['product_name', 'product_version', 'product_category', 'company_name']:
-        SubElement(device_info_el, key).text = device_info.get(key, '')
+    for key in ['ProductName', 'ProductVersion', 'ProductCategory', 'CompanyName']:
+        SubElement(device_info_el, key).text = device_info.get(key, '') or 'NULL'
 
     # DeviceConfiguration 추가 (endpoint, username, password 포함)
     device_config = SubElement(root, 'DeviceConfiguration')
-    SubElement(device_config, 'Endpoint').text = device_info.get('endpoint', '')
+    SubElement(device_config, 'Endpoint').text = device_info.get('endpoint', '') or 'NULL'
     auth = SubElement(device_config, 'Authentication')
-    SubElement(auth, 'Username').text = device_info.get('username', '')
-    SubElement(auth, 'Password').text = device_info.get('password', '')
+    SubElement(auth, 'Username').text = device_info.get('username', '') or 'NULL'
+    SubElement(auth, 'Password').text = device_info.get('password', '') or 'NULL'
 
     # TestConfiguration 추가
     test_config = SubElement(root, 'TestConfiguration')
@@ -115,7 +110,7 @@ def generate_xml(device_info, selected_cases, additional_settings):
 
     # 추가 설정이 있는 테스트 케이스만 XML에 추가
     for case, settings in additional_settings.items():
-        if case in sum(selected_cases.values(), []) and settings:
+        if case in sum(additional_config_test_cases.values(), []) and settings:
             case_element = SubElement(test_config, case, {'name': settings.pop('name', case)})
             for key, value in settings.items():
                 SubElement(case_element, key).text = value
